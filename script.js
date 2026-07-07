c61fa08c73b4e00001ef8d65273607faaacbeac3
Brian Ogle <Brianogle74@gmail.com>
2026-07-07 13:38:45 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const playlists = {
+    "Laundry": [ /* Add video objects here */ ],
+    "How To": [ /* Add video objects here */ ],
+    "Cleaning & Organizing": [ /* Add video objects here */ ],
+    "Cook With Me": [ /* Add video objects here */ ],
+    "Favs": [ /* Add video objects here */ ]
+};
